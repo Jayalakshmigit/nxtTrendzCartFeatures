ab9d13a3a96f1a8b5602b61883cce14ab7256f8e
jayalakshmi <jayalakshmi3454@gmail.com>
2023-10-12 14:39:39 +0000

    error

---
+++ src/components/Cart/index.js
@@ -0,0 +1,49 @@
+import Header from '../Header'
+import CartListView from '../CartListView'
+import CartSummary from '../CartSummary'
+
+import CartContext from '../../context/CartContext'
+import EmptyCartView from '../EmptyCartView'
+
+import './index.css'
+
+const Cart = () => (
+  <CartContext.Consumer>
+    {value => {
+      const {cartList, removeAll} = value
+      const showEmptyView = cartList.length === 0
+      // TODO: Update the functionality to remove all the items in the cart
+
+      const onClickRemoveAllButton = () => {
+        removeAll()
+      }
+
+      return (
+        <>
+          <Header />
+          <div className="cart-container">
+            {showEmptyView ? (
+              <EmptyCartView />
+            ) : (
+              <div className="cart-content-container">
+                <h1 className="cart-heading">My Cart</h1>
+
+                {/* TODO: Add your code for Cart Summary here */}
+                <button
+                  className="remove-all"
+                  type="button"
+                  onClick={onClickRemoveAllButton}
+                >
+                  Remove All
+                </button>
+                <CartListView />
+                <CartSummary />
+              </div>
+            )}
+          </div>
+        </>
+      )
+    }}
+  </CartContext.Consumer>
+)
+export default Cart
